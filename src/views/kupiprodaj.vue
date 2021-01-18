3b0682143e691c6bca2609cb5df7e1624921b0f9
Barbara Zeljeznik <barbara.zeljeznik@gmail.com>
2021-01-18 11:27:56 +0100

    Dodan kupi/prodaj

---
+++ src/views/kupiprodaj.vue
@@ -0,0 +1,385 @@
+<template>
+    <div>
+ <div class="navigacija">
+	
+    <nav class="navbar navbar-expand-xl navbar-dark bg-dark">
+      <img src="assets/logo.png" class="logo">  		
+      <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <!-- Collection of nav links, forms, and other content for toggling -->
+      <div id="navbarCollapse" class="collapse navbar-collapse justify-content-start">		
+        
+        <div class="navbar-nav">
+          <a href="/pocetna" class="razmakni activee"><img class="ikone" src="assets/pocetna_.png"><i class="fa"></i><span class="tekst_ikone">Početna</span></a>
+          <a href="/kupiprodaj" class="razmakni"><img class="ikone" src="assets/kupiprodaj.png"><i class="fa"></i><span class="tekst_ikone">Kupi/Prodaj</span></a>
+          <a href="/recenzije" class="razmakni"><img class="ikone" src="assets/recenzije_.png"><i class="fa"></i><span class="tekst_ikone">Recenzije</span></a>
+          <a href="/tips_tricks" class="razmakni"><img class="ikone" src="assets/tips&tricks_.png"><i class="fa"></i><span class="tekst_ikone">Tips&Tricks</span></a>
+          <a href="/onama" class="razmakni"><img class="ikone" src="assets/onama_.png"><i class="fa "></i><span class="tekst_ikone">O nama</span></a>
+          <a href="/profil" class="razmakni"><img class="ikone" src="assets/profil_.png"><i class="fa"></i><span class="tekst_ikone">Profil</span></a>
+          <a href="#" @click.prevent="odjava()" class="razmakni desno"><img class="ikone" src="assets/odjava.png"><i class="fa"></i><span class="tekst_ikone">Odjava</span></a>
+        </div>
+      </div>
+    </nav>
+ </div>
+ <div class="col-12">
+
+<div class="objavabtn">
+ <input v-model="objava" type="text" class="objava" placeholder="Dodaj objavu..." id="objava"  data-toggle="modal" data-target="#ModalLoginForm" >
+ <button type="button" class="btn1 btn-primary btn-lg" data-toggle="modal" data-target="#ModalLoginForm" > Objavi</button>
+</div>
+
+<div id="ModalLoginForm" class="modal fade">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title">Dodavanje proizvoda</h1>
+            </div>
+            <div class="modal-body">
+                <form role="form" method="POST" action="">
+                    <input type="hidden" name="_token" value="">
+                    <div class="form-group">
+                        <label class="control-label">Lokacija</label>
+                        <div>
+                            <input type="text" class="form-control input-lg" name="location" value="">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Kontakt</label>
+                        <div>
+                            <input type="text" class="form-control input-lg" name="contact">
+                        </div>
+                    </div>
+                </form>
+                <form role="form" method="POST" action="">
+                    <input type="hidden" name="_token" value="">
+                    <div class="form-group">
+                        <label class="control-label">Stanje</label>
+                        <div>
+                            <select class="form-control input-lg" name="stanje" value="">
+                                <option value="novo"> novo </option>
+                                <option value="rabljeno"> rabljeno </option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Cijena</label>
+                        <div>
+                            <input type="text" class="form-control input-lg" name="cijena" value="">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Kratak opis slike</label>
+                        <div>
+                            <input type="text" class="form-control input-lg" name="opis">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div>
+                            <button type="button" class="btn">
+                                Objavi
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+    
+ <!--<form @submit.prevent="postNewImage" class="form-inline mb-5">
+    <div class="form-group">
+        <label for="imageUrl">Image URL</label>
+     <input
+         v-model="newImageUrl"
+        type="text"
+        class="form-control ml-2"
+        placeholder="Enter the image URL"
+        id="imageUrl"
+        />
+</div>
+<div class="form-group">
+<label for="imageDescription">Description</label>
+    <input
+    v-model="newImageDescription"
+        type="text"
+        class="form-control ml-2"
+        placeholder="Enter the image description"
+        id="imageDescription"
+        />
+</div>
+<button type="submit" class="btn btn-primary ml-2">Post image</button>
+</form> -->
+ </div>
+    </div>
+</template>
+
+<script>
+
+function updateParent() {
+    opener.document.parentForm.pf1.value = document.childForm.cf1.value;
+    opener.document.parentForm.pf2.value = document.childForm.cf2.value;
+    if (document.childForm.cf3[0].checked)
+       opener.document.parentForm.pf3[0].checked = true;
+    if (document.childForm.cf3[1].checked)
+       opener.document.parentForm.pf3[1].checked = true;       
+    self.close();
+    return false;
+}
+</script>
+
+
+<style>
+
+ .navbar {
+	color: #fff;
+	background: rgba(1, 1, 1, 0.699) !important;
+	opacity:100%;
+	padding: 5px 16px;
+	border-radius: 0;
+	border: none;
+  	box-shadow: 0 0 4px rgba(0,0,0,.1);
+  	font-family: 'Playfair Display', serif;
+}
+.navbar .ikone {
+	border-radius: 50%;
+	width: 36px;
+	height: 36px;
+	margin-right: 10px;
+}
+.navbar .navbar-brand {
+	color: #efe5ff;
+	padding-left: 0px;
+	padding-right: 50px;
+	font-size: 24px;
+	
+		
+}
+.navbar-nav{
+	padding-left:25px;
+	flex-grow:1;
+	
+}
+.navbar .navbar-brand:hover, .navbar .navbar-brand:focus {
+  color: #fff;
+  opacity:50%;
+}
+.navbar .navbar-brand i {
+	font-size: 25px;
+  margin-right: 5px;
+}
+.search-box {
+	position: relative;
+}	
+
+
+.search-box i {
+	color: #a0a5b1;
+	font-size: 19px;
+}
+.navbar .nav-item i {
+	font-size: 18px;
+}
+.navbar .nav-item span {
+	position: relative;
+	top: 3px;
+}
+.navbar .navbar-nav > a {
+	color: #efe5ff;
+	padding: 8px 15px;
+  font-size: 18px;	
+  
+}
+.navbar .navbar-nav > a:hover, .navbar .navbar-nav > a:focus {
+	color: #A43323;
+	text-decoration:none;
+}
+.navbar .navbar-nav > a > i {
+	display: block;
+	text-align: center;
+}
+.navbar .dropdown-item i {
+	font-size: 16px;
+	min-width: 22px;
+}
+.navbar .dropdown-item .material-icons {
+	font-size: 21px;
+	line-height: 16px;
+	vertical-align: middle;
+	margin-top: -2px;
+}
+.navbar .nav-item.open > a, .navbar .nav-item.open > a:hover, .navbar .nav-item.open > a:focus {
+	color: #fff;
+	background: none !important;
+}
+.navbar .dropdown-menu {
+	border-radius: 1px;
+	border-color: #e5e5e5;
+	box-shadow: 0 2px 8px rgba(0,0,0,.05);
+}
+.navbar .dropdown-menu a {
+	color: #777 !important;
+	padding: 8px 20px;
+	line-height: normal;
+	font-size: 15px;
+}
+.navbar .dropdown-menu a:hover, .navbar .dropdown-menu a:focus {
+	color: #333 !important;
+	
+}
+.navbar .navbar-nav .active a, .navbar .navbar-nav .active a:hover, .navbar .navbar-nav .active a:focus {
+	color: #fff;
+  text-shadow: 0 0 4px rgba(255,255,255,0.2);
+  text-decoration: none;
+	
+}
+.navbar .navbar-nav .user-action {
+	padding: 9px 15px;
+	font-size: 15px;
+}
+.navbar .navbar-toggle {
+	border-color: #fff;
+}
+.navbar .navbar-toggle .icon-bar {
+	background: #fff;
+}
+/*.navbar .navbar-toggle:focus, .navbar .navbar-toggle:hover {
+	
+}*/
+.navbar .navbar-nav .open .dropdown-menu {
+	background: #faf7fd;
+	border-radius: 1px;
+	border-color: #faf7fd;
+	box-shadow: 0 2px 8px rgba(0,0,0,.05);
+}
+.navbar .divider {
+	background-color: #e9ecef !important;
+}
+@media (min-width: 1200px){
+	.form-inline .input-group {
+		width: 350px;
+		margin-left: 30px;
+	}
+}
+@media (max-width: 1199px){
+	.navbar .navbar-nav > a > i {
+		display: inline-block;			
+		text-align: left;
+		min-width: 30px;
+		position: relative;
+		top: 4px;
+	}
+	.navbar .navbar-collapse {
+		border: none;
+		box-shadow: none;
+		padding: 0;
+	}
+	.navbar .navbar-form {
+		border: none;			
+		display: block;
+		margin: 10px 0;
+		padding: 0;
+	}
+	.navbar .navbar-nav {
+		margin: 8px 0;
+	}
+	.navbar .navbar-toggle {
+		margin-right: 0;
+	}
+	.input-group {
+		width: 100%;
+  }
+  
+}
+
+.razmakni{
+  margin-left: 5px;
+  display:inline-flex;
+  
+}
+.ikone{
+  display: inline-block;
+
+}
+.logo{
+	border-radius: 50%;
+	width: 120px;
+	height: 45px;
+	margin-right: 10px;
+}
+.tekst_ikone{
+	display:inline-block;
+	margin-right:10px;
+	margin-top:8px;
+}
+
+.activee{
+	color: #c73500a6;
+}
+.desno {
+	right:0%;
+	position:absolute;
+}
+
+.objava {
+    margin-top:2%;
+    font-size:20px;
+    font-family:'Playfair Display', serif ;
+    padding-bottom:7.5px;
+    
+
+}
+
+.modal-header {
+font-family:'Playfair Display', serif ;
+margin-left:9%;
+
+}
+
+.control-label {
+    float: left;
+    margin-left: 1%;
+    font-family:'Playfair Display', serif ;
+}
+
+.btn {
+    font-family:'Playfair Display', serif ;
+    background-color: #c73500a6;
+    border:none;
+    color:white;
+}
+
+.btn:hover {
+    border:none;
+    background-color:#ce623bd7 ;
+    color:white;
+}
+
+.btn:active {
+    border:none;
+    color:#ce623bd7 ;
+    color:white;
+
+}
+
+.btn1 {
+ background-color:#ce623bd7 ;
+ border:none;
+ margin-left:2%;
+ font-size: 14px;
+
+}
+
+.btn1:hover {
+    border:none;
+    background-color:#ce623bd7 ;
+    color:white; 
+}
+
+.btn1:active {
+    border:none;
+    background-color:#ce623bd7 ;
+    color:white;
+}
+
+</style>
