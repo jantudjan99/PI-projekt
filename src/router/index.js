90d9eb78a20813ef15a0cafcf873b50eca58afa3
Barbara Zeljeznik <barbara.zeljeznik@gmail.com>
2021-02-04 19:06:47 +0100

    Finalna verzija index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Prva from '../views/prva.vue'
+import store from "@/store"
 Vue.use(VueRouter)
 
 const routes = [
@@ -13,46 +14,68 @@ const routes = [
   {
     path: '/registracija',
     name: 'registracija',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/registracija.vue')
+    component: () => import('../views/registracija.vue')
   },
   {
     path: '/pocetna',
     name: 'pocetna',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/pocetna.vue')
+    component: () => import( '../views/pocetna.vue'),
+    meta: {
+      needsUser: true
+    }
   },
   {
   path: '/onama',
   name: 'onama',
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  component: () => import(/* webpackChunkName: "about" */ '../views/onama.vue')
+  component: () => import('../views/onama.vue'),
+  meta: {
+    needsUser: true
+  }
 },
   
 {
   path: '/prijava',
   name: 'prijava',
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  component: () => import(/* webpackChunkName: "about" */ '../views/prijava.vue')
+  component: () => import('../views/prijava.vue'),
 },
 
 {
   path: '/kupiprodaj',
   name: 'kupiprodaj',
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  component: () => import(/* webpackChunkName: "about" */ '../views/kupiprodaj.vue')
+  component: () => import('../views/kupiprodaj.vue'),
+  meta: {
+    needsUser: true
+  }
 },
 
+{
+  path: '/recenzije',
+  name: 'recenzije',
+
+  component: () => import( '../views/recenzije.vue'),
+  meta: {
+    needsUser: true
+  }
+},
+
+{
+  path: '/tips_tricks',
+  name: 'tips_tricks',
+
+  component: () => import('../views/tips_tricks.vue'),
+  meta: {
+    needsUser: true
+  }
+},
+
+{
+  path: '/profil',
+  name: 'profil',
+  component: () => import( '../views/profil.vue'),
+  meta: {
+    needsUser: true
+  }
+}
 
 
 ]
@@ -63,5 +86,30 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+/*const handleUpload = () =>{ const uploadTask = storage.ref('images/${image.name}').put(image); 
+  uploadTask.on( 
+      "state changed", 
+      snapshot => {}, 
+      error => { console.log(error); 
+    },
+    () => { 
+        storage 
+        .ref("Objave") 
+        .child(image.name) 
+        .getDownloadURL() 
+        .then(url => { // console.log(url); 
+  }); 
+    }
+  );
+};*/
+
+router.beforeEach((to, from, next)=>{
+
+
+  next();
+})
+
+
 
 export default router
